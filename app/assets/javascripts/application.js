4a3af12dec0ee6cb0a59cb55152d837238860e8b
Marcin Szot <marcin.szot89@gmail.com>
2014-12-02 22:12:28 +0100

    Stickiness of the categories menu added.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 
 //= require_tree .
+
+$(document).ready(function() {
+    $(".categories_bar").wrap('<div class = "categories_placeholder"></div>');
+    $(".categories_placeholder").height($(".categories_bar").outerHeight());
+
+    var scrollTop = 0;
+    var menuOffset = $(".categories_bar").offset().top;
+    $(window).scroll(function() {
+        scrollTop = $(window).scrollTop();
+        if(scrollTop >= menuOffset) {
+            $(".categories_bar").addClass("fixed");
+        } else {
+            $(".categories_bar").removeClass("fixed");
+        }
+    });
+});
